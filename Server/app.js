3633ab16e7c9cce8a06829ab21ac4709c2e2dc71
Aspen Duvall <aspenduvall@gmail.com>
2021-08-31 21:08:46 -0400

    finished server

--- Server/app.js
+++ Server/app.js
@@ -1,6 +1,23 @@
+require("dotenv").config();
 let express = require('express');
 let app = express();
+let sequelize = require('./Db');
+
+let food = require("./Controllers/Food-controller");
+let user = require("./Controllers/User-controller");
+
+sequelize.sync();
+
+app.use(require("./Middleware/Headers"));
+
+app.use(express.json());
+
+app.use("/food", food);
+
+app.use("/user", user);
+
+app.use(require("./Middleware/Validate-session"));
 
 app.listen(3000, function(){
     console.log('App is listening on port 3000')
-})
+});
