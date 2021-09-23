4f4cefef3ebbf27dcc78631d73fb38e4ed8d679f
James Drake <jbdrake42@gmail.com>
2021-09-23 19:46:51 -0400

    first server heroku push

--- Server/Db.js
+++ Server/Db.js
@@ -1,6 +1,5 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('I8That', 'postgres', 'password', {
-    host: 'localhost',
+const sequelize = new Sequelize(process.env.DATABASE_URL,{
     dialect: 'postgres'
 });
 
